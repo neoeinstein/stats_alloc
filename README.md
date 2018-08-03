3d1f244bb26c906d94df2d73be1f9543ad6ffc8d
Marcus Griep <marcus@griep.us>
2018-08-03 12:19:07 -0400

    Add initial docs

--- README.md
+++ README.md
@@ -4,4 +4,22 @@ An instrumenting middleware for global allocators in Rust, useful in testing
 for validating assumptions regarding allocation patterns, and potentially in
 production loads to monitor for memory leaks.
 
- 
+## Example
+
+```rust
+extern crate stats_alloc;
+
+use stats_alloc::{StatsAlloc, Region};
+use std::alloc::System;
+
+#[global_allocator]
+static STATS_ALLOC: StatsAlloc<System> = StatsAlloc::system();
+
+fn example_using_region() {
+    let reg = Region::new(&STATS_ALLOC);
+    let x: Vec<u8> = Vec::with_capacity(1_024);
+    println!("Stats at 1: {:#?}", reg.change());
+    // Used here to esnure that the value isn't deallocated first
+    ::std::mem::size_of_val(&x);
+}
+``` 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+//!
+//! An instrumenting middleware for global allocators in Rust, useful in testing
+//! for validating assumptions regarding allocation patterns, and potentially in
+//! production loads to monitor for memory leaks.
+//!
+//! ## Example
+//!
+//! ```
+//! extern crate stats_alloc;
+//!
+//! use stats_alloc::{StatsAlloc, Region};
+//! use std::alloc::System;
+//!
+//! #[global_allocator]
+//! static STATS_ALLOC: StatsAlloc<System> = StatsAlloc::system();
+//!
+//! fn main() {
+//!     let reg = Region::new(&STATS_ALLOC);
+//!     let x: Vec<u8> = Vec::with_capacity(1_024);
+//!     println!("Stats at 1: {:#?}", reg.change());
+//!     // Used here to esnure that the value isn't deallocated first
+//!     ::std::mem::size_of_val(&x);
+//! }
+//! ```
+
 #![feature(const_fn)]
 
 use std::alloc::{GlobalAlloc, Layout, System};
